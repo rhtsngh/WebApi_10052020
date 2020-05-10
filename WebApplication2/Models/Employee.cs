15162fdd3e3bc8bae0c1ce71fd53adfaeafc65d7
Rohit <rohit.jangwal@gmail.com>
2020-05-10 13:02:59 +0530

    importing project

---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeName { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ WebApplication2/Models/SchoolSimsContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication2.Models
+{
+    public partial class SchoolSimsContext : DbContext
+    {
+        public SchoolSimsContext()
+        {
+        }
+
+        public SchoolSimsContext(DbContextOptions<SchoolSimsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:schoolsims.database.windows.net,1433;Initial Catalog=SchoolSims;Persist Security Info=False;User ID=rrohitx;Password=Krish@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Department).HasMaxLength(50);
+
+                entity.Property(e => e.EmployeName).HasMaxLength(50);
+            });
+        }
+    }
+}
