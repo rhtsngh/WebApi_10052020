d605308b2177833a265863cce61c089d56ac11bb
Rohit <rohit.jangwal@gmail.com>
2020-05-10 13:08:06 +0530

    new change

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET api/values/1
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
